03b1b405bd8ae6db4a07a86802bf261004f0bf6f
Cesar Lara <cesar.lara@yale.edu>
2020-05-21 05:26:05 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -6,9 +6,12 @@ def second_challenge
       :hero => {},
       :hero_friends => []
       },
+
+      
     :capulet => {
-
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends []
       }}
-
-
 end
